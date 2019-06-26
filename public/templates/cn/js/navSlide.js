c857e57da9a16c5f20debd00e887fd22c36620f8
dyggod <dyggod@163.com>
2019-06-26 09:31:39 +0800

    second

---
+++ public/templates/cn/js/navSlide.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $('#navSlide b').click(function () {
+        var sub = $(this).parents('li.nli').children('.sub');
+        if ($(this).hasClass('hover')) {
+            $(this).removeClass('hover');
+            sub.slideUp();
+        } else {
+            $(this).addClass('hover');
+            sub.slideDown();
+        }
+    });
+});
